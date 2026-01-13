04ac4ebf6a5ca71dd450859d5d186bbef03ec21b
nguyenduyy <duyy.uit@gmail.com>
2026-01-13 10:54:50 +0700

    add about & policy pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "794066461"
+    serviceWorkerVersion: "3756185432"
   }
 });
